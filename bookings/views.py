0151723aeebbeb5c4435c7fc91d7ea7105df5de7
uglyfeet <welch.antony@gmail.com>
2026-09-14 22:23:13 +0100

    Add booking homepage template and URL

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Room
 
-# Create your views here.
+
+def home(request):
+    rooms = Room.objects.filter(available=True)
+    return render(request, "bookings/home.html", {"rooms": rooms})
